3913257e5f63296c43a632a390001d6dcc31314c
Vinay KN <vinaykn400@gmail.com>
2026-04-27 01:01:20 +0530

    Fix BASE_URL declaration in api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = ""https://urban-sim-backend-production.up.railway.app/api"";
+const BASE_URL = "https://urban-sim-backend-production.up.railway.app/api";
 
 export const api = {
   // NODES
